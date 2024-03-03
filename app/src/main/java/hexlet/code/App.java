befca9926b12388fcaf785a4db1d4e881b1b3c21
Naumenko Tatiana <jouk@list.ru>
2024-03-03 20:20:56 +0300

    Create Differ.generate() programme

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,20 +4,27 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-import java.io.File;
+
+import java.util.concurrent.Callable;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable {
+public class App implements Callable<String> {
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String format = "stylish";
 
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
-    private File filepath1;
+    String filepath1;
     @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
-    private File filepath2;
+    String filepath2;
     @Override
-    public void run() {
-        System.out.println("Hi");
+    public String call() {
+        try {
+            System.out.println(Differ.generate(filepath1, filepath2));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,62 @@
+package hexlet.code;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Differ {
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        Path file1Path = Paths.get(filePath1).toAbsolutePath().normalize();
+        Path file2Path = Paths.get(filePath2).toAbsolutePath().normalize();
+        if (!Files.exists(file1Path)) {
+            throw new Exception("File '" + file1Path + "' does not exist");
+        }
+        if (!Files.exists(file2Path)) {
+            throw new Exception("File '" + file2Path + "' does not exist");
+        }
+
+        String jsonFile1 = Files.readString(file1Path);
+        String jsonFile2 = Files.readString(file2Path);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map1 = objectMapper.readValue(jsonFile1, new TypeReference<>(){});
+        Map<String, Object> map2 = objectMapper.readValue(jsonFile2, new TypeReference<>(){});
+
+        Set<String> set = new TreeSet<>();
+        set.addAll(map1.keySet());
+        set.addAll(map2.keySet());
+
+        Map<String, String> resultMap = new LinkedHashMap<>();
+
+        for (String key : set) {
+            if (map1.containsKey(key) && !map2.containsKey(key)) {
+                String newKey = "- " + key;
+                resultMap.put(newKey, map1.get(key).toString());
+            } else if (map2.containsKey(key) && !map1.containsKey(key)) {
+                String newKey = "+ " + key;
+                resultMap.put(newKey, map2.get(key).toString());
+            } else if (map2.containsKey(key) && map1.containsKey(key)) {
+                if (map1.get(key).equals(map2.get(key))) {
+                    String newKey = "  " + key;
+                    resultMap.put(newKey, map1.get(key).toString());
+                } else {
+                    String newKey1 = "- " + key;
+                    resultMap.put(newKey1, map1.get(key).toString());
+                    String newKey2 = "+ " + key;
+                    resultMap.put(newKey2, map2.get(key).toString());
+                }
+            }
+        }
+
+        String result = resultMap.entrySet()
+                .stream()
+                .map(e -> e.getKey() + ": " + e.getValue())
+                .collect(Collectors.joining("\n"));
+        return "{\n" + result + "\n}";
+    }
+}
