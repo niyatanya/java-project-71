d1ae6e74a96be123656b303283e8fd69d538776e
Naumenko Tatiana <jouk@list.ru>
2024-03-10 19:51:08 +0300

    Revise DifferTest.java and add Empty.json for tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,38 +2,25 @@ package hexlet.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-//import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 class DifferTest {
-    private String jsonFile1;
-    private String jsonFile2;
-    private String expected;
-
-    @BeforeEach
-    public void prepare() throws Exception {
-        Path file1Path = Paths.get("src/test/resources/file1.json").toAbsolutePath().normalize();
-        jsonFile1 = Files.readString(file1Path);
-        Path file2Path = Paths.get("src/test/resources/file2.json").toAbsolutePath().normalize();
-        jsonFile2 = Files.readString(file2Path);
-        if (!Files.exists(file1Path)) {
-            throw new Exception("File '" + file1Path + "' does not exist");
-        }
-        if (!Files.exists(file2Path)) {
-            throw new Exception("File '" + file2Path + "' does not exist");
-        }
-        expected = "{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n"
-                + "+ timeout: 20\n+ verbose: true\n}";
-    }
 
     @Test
     public void testGenerate() throws Exception {
-        String actual = Differ.generate(jsonFile1, jsonFile2);
+        String file1Path = "src/test/resources/file1.json";
+        String file2Path = "src/test/resources/file2.json";
+        String expected = "{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n"
+                + "+ timeout: 20\n+ verbose: true\n}";
+        String actual = Differ.generate(file1Path, file2Path);
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testGenerateEmptyFile() throws Exception {
+        String file1Path = "src/test/resources/file1.json";
+        String emptyFilePath = "src/test/resources/Empty.json";
+        String expected = "{\n- follow: false\n- host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n}";
+        String actual = Differ.generate(file1Path, emptyFilePath);
+        assertEquals(expected, actual);
+    }
 }
