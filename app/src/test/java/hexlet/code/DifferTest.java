e78a0998dbac15aa341423770525a28b50f172c5
Naumenko Tatiana <jouk@list.ru>
2024-03-20 00:17:06 +0300

    Add emply file test

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -38,14 +38,27 @@ class DifferTest {
         assertEquals(expected, actual);
     }
 
-//    @Test
-//    public void testGenerateEmptyFile() throws Exception {
-//        String file1Path = "src/test/resources/file1.json";
-//        String emptyFilePath = "src/test/resources/Empty.json";
-//        String expected = "{\n- follow: false\n- host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n}";
-//        String actual = Differ.generate(file1Path, emptyFilePath);
-//        assertEquals(expected, actual);
-//    }
+    @Test
+    public void testGenerateEmptyFile() throws Exception {
+        String file1Path = "src/test/resources/file1.json";
+        String emptyFilePath = "src/test/resources/Empty.json";
+        String expected = "{\n"
+                + "- chars1: [a, b, c]\n"
+                + "- chars2: [d, e, f]\n"
+                + "- checked: false\n"
+                + "- default: null\n"
+                + "- id: 45\n"
+                + "- key1: value1\n"
+                + "- numbers1: [1, 2, 3, 4]\n"
+                + "- numbers2: [2, 3, 4, 5]\n"
+                + "- numbers3: [3, 4, 5]\n"
+                + "- setting1: Some value\n"
+                + "- setting2: 200\n"
+                + "- setting3: true\n"
+                + "}";
+        String actual = Differ.generate(file1Path, emptyFilePath, "stylish");
+        assertEquals(expected, actual);
+    }
 
     @Test
     public void testGenerateStylishYml() throws Exception {
